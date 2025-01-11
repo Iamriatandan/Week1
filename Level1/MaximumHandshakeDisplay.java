a767dcdcce9d7b1a27f4f99a1baf4d11cc1eb7c0
iamriatandan <tandanria@gmail.com>
2025-01-11 13:28:56 +0530

    question 3

--- Level1/MaximumHandshakeDisplay.java
+++ Level1/MaximumHandshakeDisplay.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class MaximumHandshake {
+class MaximumHandshakeDisplay {
     public static void main(String[] args) {
         // Creating scanner class object for input
         Scanner input = new Scanner(System.in);
@@ -8,13 +8,17 @@ class MaximumHandshake {
 		//input for number of students
 		System.out.println("Enter the number of students");
 		int students = input.nextInt();
-       
+
+		//creating object for display
+		MaximumHandshakeDisplay handshake = new MaximumHandshakeDisplay();
+		
+		System.out.println("The maximum handshakes among " + students + " is " + handshake.handShakes(students));
         // Closing the scanner resource
         input.close();
     }
 
     // Method to calculate handshakes
-    public  static int handShakes(int student) {
-        return (student*(student-1))/2;
+    public  int handShakes(int students) {
+        return (students*(students-1))/2;
     }
 }
